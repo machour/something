67327f7624139ef2c91ed3ded5ba1dd1837f079e
Mehdi Achour <machour@gmail.com>
2023-05-09 00:16:21 +0100

    Prettier

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Toying with some ideas of [Yii](https://www.yiiframework.com) on NodeJS.
+
+[![CI](https://github.com/machour/something/actions/workflows/test.yml/badge.svg)](https://github.com/machour/something/actions/workflows/test.yml)
--- base/BaseObject.js
+++ base/BaseObject.js
@@ -2,95 +2,113 @@ import { InvalidCallException } from "./InvalidCallException";
 import { UnknownPropertyException } from "./UnknownPropertyException";
 
 function uppercaseFirst(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 export class BaseObject {
-    className() {
-        return this.constructor.name;
-    }
-    static className() {
-        return this.constructor.name;
-    }
-    constructor() {
-        return new Proxy(this, {
-            /**
-             * Sets value of an object property.
-             *
-             * @param {object} instance the current instance
-             * @param {string} key the property name
-             * @param {*} value the property value
-             * @throws {UnknownPropertyException} if the property is not defined
-             * @throws {InvalidCallException} if the property is read-only
-             */
-            set(instance, key, value) {
-                const setter = `set${uppercaseFirst(key)}`;
-                if (typeof instance[setter] === "function") {
-                    instance[setter](value);
-                    return true;
-                }
+  /**
+   * Gets the current class name
+   *
+   * @returns string
+   */
+  className() {
+    return this.constructor.name;
+  }
+  constructor() {
+    return new Proxy(this, {
+      /**
+       * Sets value of an object property.
+       *
+       * @param {object} instance the current instance
+       * @param {string} key the property name
+       * @param {*} value the property value
+       * @throws {UnknownPropertyException} if the property is not defined
+       * @throws {InvalidCallException} if the property is read-only
+       */
+      set(instance, key, value) {
+        const setter = `set${uppercaseFirst(key)}`;
+        if (typeof instance[setter] === "function") {
+          instance[setter](value);
+          return true;
+        }
 
-                const getter = `get${uppercaseFirst(key)}`;
-                if (typeof instance[getter] === "function") {
-                    throw new InvalidCallException(
-                        `Setting read-only property: ${instance.constructor.name}::${key}`
-                    );
-                }
+        const getter = `get${uppercaseFirst(key)}`;
+        if (typeof instance[getter] === "function") {
+          throw new InvalidCallException(
+            `Setting read-only property: ${instance.constructor.name}::${key}`
+          );
+        }
 
-                throw new UnknownPropertyException(
-                    `Setting unknown property: ${instance.constructor.name}::${key}`
-                );
-            },
-            /**
-             * Returns the value of an object property.
-             *
-             * @param {object} instance the current instance
-             * @param {string} key the property name
-             * @return {*} the property value
-             * @throws {UnknownPropertyException} if the property is not defined
-             * @throws {InvalidCallException} if the property is write-only
-             * @see __set()
-             */
-            get(instance, key) {
-                if (typeof instance[key] !== "undefined") {
-                    return instance[key];
-                }
+        throw new UnknownPropertyException(
+          `Setting unknown property: ${instance.constructor.name}::${key}`
+        );
+      },
+      /**
+       * Returns the value of an object property.
+       *
+       * @param {object} instance the current instance
+       * @param {string} key the property name
+       * @return {*} the property value
+       * @throws {UnknownPropertyException} if the property is not defined
+       * @throws {InvalidCallException} if the property is write-only
+       */
+      get(instance, key) {
+        if (typeof instance[key] !== "undefined") {
+          return instance[key];
+        }
 
-                const getter = `get${uppercaseFirst(key)}`;
-                if (typeof instance[getter] === "function") {
-                    return instance[getter]();
-                }
+        const getter = `get${uppercaseFirst(key)}`;
+        if (typeof instance[getter] === "function") {
+          return instance[getter]();
+        }
 
-                const setter = `set${uppercaseFirst(key)}`;
-                if (typeof instance[setter] === "function") {
-                    throw new InvalidCallException(
-                        `Getting write-only property: ${instance.constructor.name}::${key}`
-                    );
-                }
+        const setter = `set${uppercaseFirst(key)}`;
+        if (typeof instance[setter] === "function") {
+          throw new InvalidCallException(
+            `Getting write-only property: ${instance.constructor.name}::${key}`
+          );
+        }
 
-                throw new UnknownPropertyException(
-                    `Getting unknown property: ${instance.constructor.name}::${key}`
-                );
-            },
-            has(instance, key) {
-                return typeof instance[key] !== "undefined" || typeof instance[`get${uppercaseFirst(key)}`] === "function"
-            },
-            deleteProperty(instance, key) {
-                const setter = `set${uppercaseFirst(key)}`;
-                if (typeof instance[setter] === "function") {
-                    instance[setter](null);
-                    return true;
-                }
+        throw new UnknownPropertyException(
+          `Getting unknown property: ${instance.constructor.name}::${key}`
+        );
+      },
+      /**
+       * Handles `"name" in instance`
+       *
+       * @param {object} instance the current instance
+       * @param {string} key the property name
+       * @returns {boolean}
+       */
+      has(instance, key) {
+        return (
+          typeof instance[key] !== "undefined" ||
+          typeof instance[`get${uppercaseFirst(key)}`] === "function"
+        );
+      },
+      /**
+       * Handles `delete instance.name`
+       *
+       * @param {object} instance the current instance
+       * @param {string} key the property name
+       * @returns {boolean}
+       */
+      deleteProperty(instance, key) {
+        const setter = `set${uppercaseFirst(key)}`;
+        if (typeof instance[setter] === "function") {
+          instance[setter](null);
+          return true;
+        }
 
-                const getter = `get${uppercaseFirst(key)}`;
-                if (typeof instance[getter] === "function") {
-                    throw new InvalidCallException(
-                        `Unsetting read-only property: ${instance.constructor.name}::${key}`
-                    );
-                }
+        const getter = `get${uppercaseFirst(key)}`;
+        if (typeof instance[getter] === "function") {
+          throw new InvalidCallException(
+            `Unsetting read-only property: ${instance.constructor.name}::${key}`
+          );
+        }
 
-                return true;
-            }
-        });
-    }
+        return true;
+      },
+    });
+  }
 }
