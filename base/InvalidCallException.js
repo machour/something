913b46a2b0a7e98759ae0fcc3ac0f0add5e82145
Mehdi Achour <machour@gmail.com>
2023-05-08 23:54:52 +0100

    Initial checkin

---
+++ base/InvalidCallException.js
@@ -0,0 +1,3 @@
+import { BaseException } from "./BaseException";
+
+export class InvalidCallException extends BaseException {}
---
+++ test/BaseObject.test.js
@@ -0,0 +1,60 @@
+import { assert, expect, test } from 'vitest'
+import { BaseObject } from "../base/BaseObject";
+import { InvalidCallException } from "../base/InvalidCallException";
+import { UnknownPropertyException } from "../base/UnknownPropertyException";
+
+class User extends BaseObject {
+    id=  42;
+    __password = "";
+    __name = "Mehdi"
+    getName() {
+        return this.__name;
+    }
+    setName(name) {
+        this.__name = name;
+    }
+    setPassword(password) {
+        this.__password = password;
+    }
+    getReadOnly() {
+        return "can't write this"
+    }
+}
+
+test('BaseObject.get', () => {
+    const user = new User();
+
+    //expect(User.className()).toBe("User")
+    expect(user.className()).toBe("User")
+    expect(user.id).toBe(42)
+    expect(user.name).toBe("Mehdi")
+    expect(() => user.password).toThrowError(InvalidCallException);
+    expect(() => user.unknown).toThrowError(UnknownPropertyException);
+})
+
+test('BaseObject.set', () => {
+    const user = new User();
+
+    user.password = "new password";
+    user.name = "John";
+    expect(user.name).toBe("John");
+
+    expect(() => user.readOnly = "watch this").toThrowError(InvalidCallException);
+    expect(() => user.unknown = "foo").toThrowError(UnknownPropertyException);
+})
+
+test('BaseObject.has', () => {
+    const user = new User();
+
+    expect("name" in user).toBe(true);
+    expect("unknown" in user).toBe(false);
+})
+
+test('BaseObject.deleteProperty', () => {
+    const user = new User();
+
+    expect(delete user.name).toBe(true);
+    expect(delete user.password).toBe(true);
+    expect(() => delete user.readOnly).toThrow(InvalidCallException);
+    expect(delete user.unknown).toBe(true);
+})
